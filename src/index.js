2026747b2d81a840573394a2c5c643e957b07c85
Danil Yasakov <yasakovdanil@gmail.com>
2020-10-20 22:52:25 +0500

    feat: add solution morse-decoder task

--- src/index.js
+++ src/index.js
@@ -37,8 +37,49 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(expr) {   
+    const dot = '.'; //10
+    const def = '-'; //11
+    let str = '';
+    let resArr = [];
+            
+    let arr = expr.match(/.{1,10}/g);
+
+    for(let i = 0; i < arr.length; i++) {
+
+        let arrItem = arr[i].match(/.{1,2}/g);
+
+        for(let j = 0; j < arrItem.length; j++) {
+            if (arrItem[j] === '10') {
+            arrItem[j] = dot;
+            } else if (arrItem[j] === '11') {
+            arrItem[j] = def;
+            } else if (arrItem[j] === '00') {
+            arrItem[j] = '';
+            } else {
+            continue;
+            }
+        }
+
+        resArr = arrItem.join('').split();
+
+        for(let n = 0; n < resArr.length; n++) {
+            if (resArr[n] === '**********') {
+            resArr[n] = ' ';
+            }
+        }
+
+        resArr.forEach((item) => {
+            if (item in MORSE_TABLE) {
+            str += MORSE_TABLE[item];
+            } else if (item === ' ') {
+            str += ' ';
+            }
+        })
+    
+    }
+
+    return str;
 }
 
 module.exports = {
